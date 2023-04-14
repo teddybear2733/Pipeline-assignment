fc4f85cf265c66e4e7a6f37c18d7278c83a836b3
teddybear2733 <60638017+teddybear2733@users.noreply.github.com>
2023-04-14 14:35:45 -0400

    Update Application.cpp

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -17,7 +17,7 @@ int CoolClass::Calc(int a, int b)
     }
     else
     {
-        std::cout << "This is a test" << endl;
+        std::cout << "This is a test";
         return a + b;
     }
     
